a269f9d20439211b3de424d4c8151cc7221f64e5
Katrib_8 <ekaterina12345qwas@gmail.com>
2020-11-23 01:09:03 +0700

    four commit (сложение)

--- laba4/laba4/Program.cs
+++ laba4/laba4/Program.cs
@@ -6,7 +6,8 @@ namespace laba4
     {
         static void Main(string[] args)
         {
-            
+            double res1 = A + B;
+            Console.WriteLine("Результатом сложения А и B будет" + Convert.ToString(res1));
         }
     }
 }
